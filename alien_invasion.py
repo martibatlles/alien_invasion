39eb78488888a64c9b171097434959531fe950b3
Martí Batlles <martibatllescabre@gmail.com>
2020-05-28 15:06:04 +0200

    Refacorització dels events i de la pantalla

--- alien_invasion.py
+++ alien_invasion.py
@@ -11,27 +11,36 @@ class AlienInvasion:
         """Inicialitza el joc i crea recursos"""
         pygame.init()
         self.settings = Settings()
-        self.screen = pygame.display.set_mode(
-                (self.settings.screen_width, self.settings.screen_height))
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
         self.ship = Ship(self)
 
-    def run_game(self):
-        """Fa fincionar el loop principal pel joc"""
-        while True:
-            # Mira els events del teclat i el ratolí.
+    
+    def _check_events(self):
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit
 
-            # Redibuixa la pantalla durant el loop
-            self.screen.fill(self.settings.bg_color)
-            
-            # Dibuixa la nau espacial a la pantalla
-            self.ship.blitme()
 
-            # Mostra la finasta dibuixada més recent.
-            pygame.display.flip()
+    def _update_screen(self):
+        """Actualitza les imatges de la pantalla i les mostra"""
+        # Redibuixa la pantalla durant el loop
+        self.screen.fill(self.settings.bg_color)
+             
+        # Dibuixa la nau espacial a la pantalla
+        self.ship.blitme()
+ 
+        # Mostra la finestra dibuixada més recent.
+        pygame.display.flip()
+
+
+
+    def run_game(self):
+        """Fa funcionar el loop principal pel joc"""
+        while True:
+            self._check_events()
+            self._update_screen()
+
 
 
 def main():
